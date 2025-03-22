52d5b6aa75c0656306cc3a283de3cd3b27af94de
Mhammad-Ali <alimohammad3588@gmail.com>
2025-03-23 00:25:14 +0500

    minimal changes

--- hirefusion/src/app/page.tsx
+++ hirefusion/src/app/page.tsx
@@ -1,6 +1,12 @@
-
+import Link from "next/link";
 export default function Home() {
   return (
+    <>
+    
     <h1 className="text=2xl text-center">Welcome to HireFusion</h1>
+    <Link href="/SignUp">Signup </Link>
+    <Link href="/Login">Login </Link>
+    </>
   );
 }
+
